59d5bdfe8b583681df2b94998ca0f0e7973ed4bf
Hiten <hiten.aggarwal@texxesoft.com>
2024-09-25 21:00:14 +0530

    update index.js file

--- Backend/index.js
+++ Backend/index.js
@@ -41,8 +41,8 @@ app.use(cors({
 
 // Connect to MongoDB
 mongoose.connect(MONGO_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ 
+  
 })
   .then(() => console.log('Connected to MongoDB'))
   .catch((error) => console.error('MongoDB connection error:', error));
